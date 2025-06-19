2438e01ce11282e6d9ad67878e2627b605343ae5
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2025-06-19 23:38:06 +0530

    Db Connect

---
+++ src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB = async () => {
+  try {
+    const mongodbUri = process.env.MONGO_URI + `/${DB_NAME}`;
+    const connection = await mongoose.connect(mongodbUri);
+    console.log(`mongoDB collect at HOST :: ${connection}`);
+  } catch (error) {
+    console.log("Error in connect DB", error);
+    process.exit(1);
+  }
+};
+
+
