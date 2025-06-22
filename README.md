61ee54106676d4a6ed54748087cb1fc4b49bfda9
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2025-06-22 23:26:14 +0530

    express configurations

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # node_backend
 - It's a node js backend setup to understand backend concepts and practice
 - practice
+
+
+## note 
+### for dotenv experiments
+`import dotenv from 'dotenv';
+dotenv.config();`
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import 'dotenv/config';
-// import dotenv from 'dotenv';
-// dotenv.config();
-import connectDB from './db/db.js';
+import "dotenv/config";
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
+const port = process.env.PORT || 5000;
 connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:3000`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error in connect : ", err);
+  });
